845cd5b029b312d4a3b4e44664be04415037ff9e
0honus0 <c397728390@gmail.com>
2020-10-13 18:22:01 +0800

    1

---
+++ auto.py
@@ -0,0 +1,39 @@
+import os
+from time import sleep
+
+lis=0
+maxnumber=0
+res=os.popen('ls')
+for data in res:
+    geshu=data.find('btfs')
+    shu=data.replace('btfs','').replace('\n','')
+    if geshu!=-1:
+        if shu=='':
+            shu=0
+        else:
+            shu=int(shu)
+        if shu > maxnumber:
+            maxnumber=shu
+print(maxnumber)
+
+os.system('wget https://raw.githubusercontent.com/0honus0/test/master/screen.py')
+while(lis<=maxnumber):
+    print('第',lis,'个')
+    sleep(2)
+    if lis==0:
+        com='sed -i s/change_this_number/0/g /root/screen.py'
+    else:
+        com='sed -i s/lis='+str(lis-1)+'/lis='+str(lis)+'/g /root/screen.py'
+    print(com)
+    os.system(com)
+    if lis==0:
+        com='screen -S btfs'
+    else:
+        com='screen -S btfs'+str(lis)
+    print(com)
+    com='python3 /root/screen.py'
+    lis+=1
+    print('wait')
+    sleep(20)
+
+    #sleep(1200)
---
+++ screen.py
@@ -0,0 +1,22 @@
+import os
+from time import sleep
+
+lis=change_this_number
+if lis==0:
+    os.environ['BTFS_PATH'] = '/root/.btfs'
+else:
+    com='/root/.btfs'+str(lis)
+    print(com)
+    os.environ['BTFS_PATH'] = com
+if lis==0:
+    os.environ['PATH'] = '${PATH}:${HOME}/btfs/bin'
+else:
+    com='${PATH}:${HOME}/btfs'+str(lis)+'/bin'
+    print(com)
+    os.environ['PATH'] = com
+if lis==0:
+    com='btfs/bin/btfs daemon'
+else:
+    com='btfs'+str(lis)+'/bin/btfs daemon'
+print(com)
+os.system(com)
