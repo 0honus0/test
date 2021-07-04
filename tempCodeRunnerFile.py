bb40035fcdd3c0d4035cadbacd3cc02513eef2aa
0honus0 <c397728390@gmail.com>
2021-07-04 16:42:22 +0800

    update

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-files=os.listdir()
-# for file in files:
-#     print(file)
-#     if 'xls' in file and file!='storage.xls' and (data[0] in file or data[1] in file):
-#         print(file)
-#         xls_file.append(file)
